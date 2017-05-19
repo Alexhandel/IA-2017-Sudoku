d02added40b294aad487e6f5bd6fc1fbda037214
Alex <alexhandel96@googlemail.com>
2017-05-19 19:30:24 -0300

    added comment

--- SudokuGenetic.py
+++ SudokuGenetic.py
@@ -12,23 +12,28 @@ numpy.random.seed()
 
 #nome do arquivo contendo a entrada
 nome_entrada="entrada.txt"
+
+#Parametros do Genetico. Mudar aqui
 initPop=100
 NGEN=50
 CXPB=0.8
 MUTPB=0.1
 
-
+#função auxiliar para conversão de string
 def RepresentsInt(s):
     try: 
         int(s)
         return True
     except ValueError:
         return False
+
+#função que acha a soma esperada em cada linha, coluna e bloco
 def findExpectedSum(size):
 	expSum = sum(range(1,size+1))
 	return expSum
 
 
+#função que retorna um vetor tabuleiro a partir da entrada em texto
 def makeBoard(boardEntry,size):
 	board = numpy.zeros((size,size))
 	i=0
@@ -45,90 +50,67 @@ def makeBoard(boardEntry,size):
 		j=0
 		i=i+1
 	return board
+#fim da função
 
-
-in_file = open(nome_entrada, 'r+')
-print ("leu arquivo " + nome_entrada)
-size=int(in_file.readline())
-boardLine=in_file.readline()
-print(size)
-thisboard = makeBoard(boardLine,size)
-print("BOARD: \n",thisboard)
-unique, counts = numpy.unique(thisboard, return_counts=True)
-emptyCount = counts[0]
-in_file.close()
-expSum=findExpectedSum(size)
-
+#Função de fitness do genetico
 def evaluate(Individual):
 	#primeira parte da função: colocar os numeros no tabuleiro de acordo com a string do individuo
-	#print("IND:  ",Individual[1])
-	#print(thisboard)
 	finalResult=0
 	tempResult=0
 	newBoard=thisboard.copy()
-	#print(newBoard,"manga")
 	genes=numpy.zeros((0,0))
-	#temp=Individual.split(' ')
-	#print(temp)
 	for x in Individual:
 		genes = numpy.append(genes,[x])
-	#print(genes)
 	indexesToSub = numpy.where(thisboard==-1)
-	#print(indexesToSub)
 	for i in range(0,len(genes)):
 		newBoard[indexesToSub[0][i]][indexesToSub[1][i]] = genes[i]
-	#print (newBoard)
 	#segunda parte da função: faz o calculo
 	#calculo por linha
 	rowResult=0
 	for i in range(0,size):
 		for j in range(0,size):
 			tempResult=tempResult+newBoard[i][j]
-		#	print ("tempResult is",tempResult, "after board", board[i][j])
 		rowResult = rowResult+ abs((tempResult-expSum))
-		#print("ROWS:",rowResult)
 		tempResult=0
 	#calculo por coluna
 	colResult=0
 	for j in range(0,size):
 		for i in range(0,size):
 			tempResult=tempResult+newBoard[i][j]
-			#print ("tempResult is",tempResult, "after board", board[i][j])
 		colResult = colResult+ abs((tempResult-expSum))
-		#print("COLUMNS", colResult)
 		tempResult=0
 	#calculo por bloco
 	blockSize=math.sqrt(size)
 	blocks=numpy.zeros((0,0))
 	blockResult=0
 	hBlocks = numpy.hsplit(newBoard,blockSize)
-	#print (hBlocks)
 	for x in hBlocks:
-		#print(x)
 		temp=numpy.vsplit(x,blockSize)
 		for f in temp:
-			#print(f,"  BANANA")
 			blocks = numpy.append(blocks,f)
-	#print(blocks)
 	for x in range(0,size):
 		for y in range (0,size):
 			tempResult=tempResult+blocks[y+(x*size)]
 		blockResult = blockResult+ abs(tempResult-expSum)
-		#print("BLOCKS:",blockResult)
 		tempResult=0
 	#avaliação final
-	#print("ROW:",rowResult)
-	#print("COLUMN:",colResult)
-	#print("BLOCK:",blockResult)
 	finalResult=float(rowResult+colResult+blockResult)+0.1
-	#print("FINAL EVAL:",finalResult)
 	return(finalResult),
 
-
+#gerador de numero aleartorio
 def genRandom():
 	return numpy.random.random_integers(1, high=size)
 
-
+in_file = open(nome_entrada, 'r+')    
+print ("leu arquivo " + nome_entrada)
+size=int(in_file.readline())		#le a primeira linha, contem o tamanho do tabuleiro
+boardLine=in_file.readline()
+thisboard = makeBoard(boardLine,size)
+print("BOARD: \n",thisboard)
+unique, counts = numpy.unique(thisboard, return_counts=True) #conta quantos espaços vazios existem
+emptyCount = counts[0]
+in_file.close()
+expSum=findExpectedSum(size)
 IND_SIZE=emptyCount
 
 creator.create("FitnessMin", base.Fitness, weights=(-10.0,))
@@ -150,15 +132,14 @@ mstats.register("min", numpy.min)
 mstats.register("max", numpy.max)
 HoF= tools.HallOfFame(1)
 
-pop= toolbox.population(n=initPop)
-for ind in pop:
+pop= toolbox.population(n=initPop)		#inicializa a população inicial
+for ind in pop:		#loop que avalia o fitness da população inicial
 	ind.fitness.values = evaluate(ind)
 
-endPop, log = algorithms.eaSimple(pop,toolbox,CXPB,MUTPB,NGEN, stats=mstats,halloffame=HoF,verbose=True)
-for ind in endPop:
+endPop, log = algorithms.eaSimple(pop,toolbox,CXPB,MUTPB,NGEN, stats=mstats,halloffame=HoF,verbose=True) #essa é a linha que de fato faz e roda o genetico
+for ind in endPop:		#loop que avalia o fitness da população final
 	ind.fitness.values = evaluate(ind)
-	#print("FITNESS:", ind.fitness.values)
-print("BEST INDIV:",HoF,"FITNESS:",evaluate(HoF))
+print("BEST INDIV:",HoF,"FITNESS:",evaluate(HoF)) #printa o individuo com melhor fitness entre todas as gerações
 print(len(endPop), initPop)
 
 
