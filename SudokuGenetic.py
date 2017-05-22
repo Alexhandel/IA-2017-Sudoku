ec5c33142f28a12f99ad9c1aa62f8cdb73914795
Alex <alexhandel96@googlemail.com>
2017-05-22 18:20:04 -0300

    added elitism

--- SudokuGenetic.py
+++ SudokuGenetic.py
@@ -102,6 +102,14 @@ def evaluate(Individual):
 def genRandom():
 	return numpy.random.random_integers(1, high=size)
 
+
+def selElitistAndTournament(individuals, k_elitist, k_tournament, tournsize):
+	return tools.selBest(individuals, k_elitist) + tools.selTournament(individuals, k_tournament, tournsize=3)
+
+
+
+
+
 in_file = open(nome_entrada, 'r+')    
 print ("leu arquivo " + nome_entrada)
 size=int(in_file.readline())		#le a primeira linha, contem o tamanho do tabuleiro
@@ -124,7 +132,8 @@ toolbox.register("individual", tools.initRepeat, creator.Individual,toolbox.attr
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate",tools.mutUniformInt,low=1,up=size, indpb=MUTPB)
-toolbox.register("select", tools.selTournament, tournsize=2)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("selectE", selElitistAndTournament, k_elitist=1, k_tournament=initPop-1, tournsize=3)
 toolbox.register("evaluate", evaluate)
 mstats = tools.Statistics(lambda ind: ind.fitness.values)
 mstats.register("avg", numpy.mean)
@@ -134,14 +143,45 @@ mstats.register("max", numpy.max)
 HoF= tools.HallOfFame(1)
 
 pop= toolbox.population(n=initPop)		#inicializa a população inicial
-for ind in pop:		#loop que avalia o fitness da população inicial
-	ind.fitness.values = evaluate(ind)
 
-endPop, log = algorithms.eaSimple(pop,toolbox,CXPB,MUTPB,NGEN, stats=mstats,halloffame=HoF,verbose=True) #essa é a linha que de fato faz e roda o genetico
-for ind in endPop:		#loop que avalia o fitness da população final
-	ind.fitness.values = evaluate(ind)
-print("BEST INDIV:",HoF,"FITNESS:",evaluate(HoF)) #printa o individuo com melhor fitness entre todas as gerações
-print(len(endPop), initPop)
+print(len(pop))
+fitnesses = list(map(toolbox.evaluate, pop))
+for ind, fit in zip(pop, fitnesses):
+	ind.fitness.values = fit
+for g in range(NGEN):
+		# Select the next generation individuals
+		offspring = toolbox.selectE(pop)
+		# Clone the selected individuals
+		offspring = list(map(toolbox.clone, offspring))
+		# Apply crossover and mutation on the offspring
+		for child1, child2 in zip(offspring[::2], offspring[1::2]):
+			if random.random() < CXPB:
+				toolbox.mate(child1, child2)
+				del child1.fitness.values
+				del child2.fitness.values
+		for mutant in offspring:
+			if random.random() < MUTPB:
+				toolbox.mutate(mutant)
+				del mutant.fitness.values
+		# Evaluate the individuals with an invalid fitness
+		invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+		fitnesses = map(toolbox.evaluate, invalid_ind)
+		for ind, fit in zip(invalid_ind, fitnesses):
+			ind.fitness.values = fit
+		pop[:] = offspring
+		fits = [ind.fitness.values[0] for ind in pop]
+		length = len(pop)
+		mean = sum(fits) / length
+		sum2 = sum(x*x for x in fits)
+		std = abs(sum2 / length - mean**2)**0.5
+		genMin=min(fits)
+		genMax=max(fits)
+		print("GENERATION: ",g+1,"   MIN: ",genMin,"   MAX: ",genMax,"   MEAN: ",mean,"   STDEV: ",std)
 
+#endPop, log = algorithms.eaSimple(pop,toolbox,CXPB,MUTPB,NGEN, stats=mstats,halloffame=HoF,verbose=True) #essa é a linha que de fato faz e roda o genetico
+#for ind in endPop:		#loop que avalia o fitness da população final
+#	ind.fitness.values = evaluate(ind)
+#print("BEST INDIV:",HoF,"FITNESS:",evaluate(HoF)) #printa o individuo com melhor fitness entre todas as gerações
+#print(len(endPop), initPop)
 
 
